92966e06434a81c3aa0945cb3309a582c0685a88
Gicheha2024 <nyanjuipatrick56@gmail.com>
2025-09-26 09:01:58 +0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,20 @@
+//simple c prgram(semester units)single line comment
+/*
+Name:Patrick Nyanjui
+Reg.No:PA107/G/28857
+Date:15 September 2025
+BSC.COMPUTER SCIENCE SEM.1 units
+*/
+int main(){
+        #include<stdio.h>
+        printf("spc 2101 Introduction to programming and problem solving\n");
+        printf("spc 2102 Introduction to computer architecture\n");
+        printf("spc 2103 Physics\n");
+        printf("spc 2104 Mathematics for science\n");
+        printf("spc 2105 Fundamentals of ICT\n");
+        printf("spc 2106 Communication and literacy skills\n");
+        printf("spc 2107 Analogue electronics\n");
+        printf("spc 2108 Discrete mathematics\n");
+        
+        return 0;
+}
