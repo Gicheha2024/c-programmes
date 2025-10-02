b8976b6d908d11734764fece42e66bc6f43b7ff3
P <nyanjuipatrick56@gmail.com>
2025-10-02 22:16:21 +0300

    Add files via upload

---
+++ Eligibility for exam.C
@@ -0,0 +1,37 @@
+/*
+Name:Patrick Nyanjui
+Reg No:CT101/G/28857/25
+Description:Eligibility-Exam
+*/
+
+   #include<stdio.h>
+  int main()
+  {
+  	//variable declaration
+  	 int average_marks;	
+  	 float attendance;
+  	 
+//prompt the user
+printf("enter your attendance in percentage:");
+scanf("%f",&attendance);
+
+printf("enter the average_marks:");
+scanf("%d",&average_marks);
+
+//calculation of eligibility 
+if(average_marks>=75 &&attendance>=40)
+{printf("you are eligible for the exam:");}else
+	
+if(average_marks<75 &&attendance<40)	
+{printf("you are not eligible for the exam:");}
+
+return 0;
+}
+	
+
+
+	
+	
+
+  	 
+  
